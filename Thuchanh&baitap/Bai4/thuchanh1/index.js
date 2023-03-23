3147f93c02098e04b36b16898dc884bafa013c99
Phuc Lam le <lephuclam08@gmail.com>
2023-03-23 16:48:59 +0700

    Buoi08

---
+++ Thuchanh&baitap/Bai4/thuchanh1/index.js
@@ -0,0 +1,6 @@
+
+ 
+function showMessage(){
+    alert('Xin chào!');
+}
+ 
---
+++ Thuchanh&baitap/Bai4/thuchanh4/index.js
@@ -0,0 +1,7 @@
+let inputWidth = prompt("Nhap vao chieu rong");
+let inputHeight= prompt("Nhap vao chieu cao");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: "+area);
+
